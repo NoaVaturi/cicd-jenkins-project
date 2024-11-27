98d4ff18623b6781d38ba368d21b42bfea6f8ae0
NoaVaturi <vaturinoah@gmail.com>
2024-11-27 12:20:14 +0200

    updated flasapp.service

--- steps.sh
+++ steps.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-mkdir -p /home/ubuntu/app
-
-cp requirements.txt /home/ubuntu/app/ 
-cp flaskapp.service /etc/systemd/system/flaskapp.service
-
-cd /home/ubuntu/app
+mkdir -p app
+cd app
 
 python3 -m venv env
 source env/bin/activate
 
-pip install -r requirements.txt
+pip install -r ../requirements.txt
+sudo cp ../flaskapp.service /etc/systemd/system/flaskapp.service
 
 sudo systemctl daemon-reload
 sudo systemctl enable flaskapp.service
